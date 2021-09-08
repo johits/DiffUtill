127d9f100efa999336744fbaca0d7a161847572c
johits <joker9302@gmail.com>
2021-09-09 00:16:31 +0900

    수정

--- app/src/main/java/com/example/test0908/BaseDiffCallback.kt
+++ app/src/main/java/com/example/test0908/BaseDiffCallback.kt
@@ -4,10 +4,13 @@ import androidx.recyclerview.widget.DiffUtil
 
 
 object BaseDiffCallback : DiffUtil.ItemCallback<DiffModel>() {
+
+    //두 개체가 동일한 항목을 나타내는지 여부를 결정하기 위해 DiffUtil에 의해 호출됩니다.
     override fun areItemsTheSame(oldItem: DiffModel, newItem: DiffModel): Boolean {
         return oldItem.diffId == newItem.diffId
     }
 
+    //두 항목에 동일한 데이터가 있는지 확인하려는 경우 DiffUtil에 의해 호출됩니다.
     override fun areContentsTheSame(oldItem: DiffModel, newItem: DiffModel): Boolean {
         return oldItem == newItem
     }
--- app/src/main/java/com/example/test0908/KeywordAdapter.kt
+++ app/src/main/java/com/example/test0908/KeywordAdapter.kt
@@ -4,7 +4,6 @@ import android.view.ViewGroup
 import com.example.test0908.databinding.RvItemBinding
 
 //ListAdapter 만들어 줌
-
  class KeywordAdapter : BaseListAdapter<Keyword, RvItemBinding>() {
 
     override fun onCreateViewHolder(
